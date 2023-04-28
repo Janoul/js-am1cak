7a13028b38d4a5b5176c91473920c044a773eac1
Janoul <111139554+Janoul@users.noreply.github.com>
2023-04-28 17:26:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-am1cak
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-am1cak)
---
+++ index.js
@@ -0,0 +1,24 @@
+const pokemonName = document.querySelector(".pokemon_name");
+const pokemonNumber = document.querySelector(".pokemon_number");
+const form = document.querySelector(".form")
+const input = document.querySelector(".input_search");
+
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  const data = await APIResponse.json();
+  return data;
+}
+
+const renderizaPokemon = async (pokemon) => {
+  const data = await fetchPokemon(pokemon);
+  pokemonName.innerHTML = `${data.name}`;
+  pokemonNumber.innerHTML = `(${data.id})`;
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  renderizaPokemon(input.value);
+  console.log(input.value);
+  input.value = ""
+})
